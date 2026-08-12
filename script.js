00115d73cc74625f59eb1b9de1d8e31a334d6671
Tara Parajuli <taranathparajuli1234@gmail.com>
2026-08-12 11:45:23 +0545

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed= new Typed("#typed-text", {
+    strings:[
+        "Full Stack Developer",
+        "UI/UX Designer"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop:true,
+    showCursor: true,
+    cursorChar:" "
+
+});
